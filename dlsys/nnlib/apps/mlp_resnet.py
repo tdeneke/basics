077bb88c52764ee813c8911e1665b260d5eeb7b4
tdeneke <mail2teddy@gmail.com>
2024-01-12 19:06:02 -0500

    mlp resnet with own lib

--- dlsys/nnlib/apps/mlp_resnet.py
+++ dlsys/nnlib/apps/mlp_resnet.py
@@ -1,6 +1,8 @@
 import sys
 
 sys.path.append("../python")
+from needle.data import DataLoader
+from needle.data.datasets import MNISTDataset
 import needle as ndl
 import needle.nn as nn
 import numpy as np
@@ -13,7 +15,10 @@ np.random.seed(0)
 
 def ResidualBlock(dim, hidden_dim, norm=nn.BatchNorm1d, drop_prob=0.1):
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    main_path = nn.Sequential(nn.Linear(dim, hidden_dim), norm(hidden_dim), nn.ReLU(), 
+                              nn.Dropout(drop_prob), nn.Linear(hidden_dim, dim), norm(dim))
+    res = nn.Residual(main_path)
+    return nn.Sequential(res, nn.ReLU())
     ### END YOUR SOLUTION
 
 
@@ -26,14 +31,37 @@ def MLPResNet(
     drop_prob=0.1,
 ):
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    resnet = nn.Sequential(nn.Linear(dim, hidden_dim), nn.ReLU(), 
+                           *[ResidualBlock(dim=hidden_dim, hidden_dim=hidden_dim//2, norm=norm, drop_prob=drop_prob) for _ in range(num_blocks)],
+                           nn.Linear(hidden_dim, num_classes))
+    return resnet
     ### END YOUR SOLUTION
 
 
 def epoch(dataloader, model, opt=None):
     np.random.seed(4)
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    tot_loss, tot_error = [], 0.0
+    loss_fn = nn.SoftmaxLoss()
+    if opt is None:    
+        model.eval()
+        for X, y in dataloader:
+            logits = model(X)
+            loss = loss_fn(logits, y)
+            tot_error += np.sum(logits.numpy().argmax(axis=1) != y.numpy())
+            tot_loss.append(loss.numpy())
+    else:
+        model.train()
+        for X, y in dataloader:
+            logits = model(X)
+            loss = loss_fn(logits, y)
+            tot_error += np.sum(logits.numpy().argmax(axis=1) != y.numpy())
+            tot_loss.append(loss.numpy())
+            opt.reset_grad()
+            loss.backward()
+            opt.step()
+    sample_nums = len(dataloader.dataset)
+    return tot_error/sample_nums, np.mean(tot_loss)
     ### END YOUR SOLUTION
 
 
@@ -48,7 +76,18 @@ def train_mnist(
 ):
     np.random.seed(4)
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    resnet = MLPResNet(28*28, hidden_dim=hidden_dim, num_classes=10)
+    opt = optimizer(resnet.parameters(), lr=lr, weight_decay=weight_decay)
+    train_set = MNISTDataset(f"{data_dir}/train-images-idx3-ubyte.gz", 
+                             f"{data_dir}/train-labels-idx1-ubyte.gz")
+    test_set = MNISTDataset(f"{data_dir}/t10k-images-idx3-ubyte.gz",
+                            f"{data_dir}/t10k-labels-idx1-ubyte.gz")
+    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_set, batch_size=batch_size)
+    for _ in range(epochs):
+        train_err, train_loss = epoch(train_loader, resnet, opt)
+    test_err, test_loss = epoch(test_loader, resnet, None)
+    return train_err, train_loss, test_err, test_loss
     ### END YOUR SOLUTION
 
 
